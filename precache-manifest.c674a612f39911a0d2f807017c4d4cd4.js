1185e69c57739e4e6bf4cd677b53d8aca2297c45
jczaplew <john@gaiagps.com>
2020-04-09 20:51:21 -0500

    Updates

---
+++ precache-manifest.c674a612f39911a0d2f807017c4d4cd4.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7086764718ca58c2b172b4daf43cb844",
+    "url": "/weather/index.html"
+  },
+  {
+    "revision": "3b651c26f718c31a6ab5",
+    "url": "/weather/static/css/main.6337e174.chunk.css"
+  },
+  {
+    "revision": "3ac42b17ccbd22fd66de",
+    "url": "/weather/static/js/2.c7fcb1ab.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/weather/static/js/2.c7fcb1ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b651c26f718c31a6ab5",
+    "url": "/weather/static/js/main.99b95894.chunk.js"
+  },
+  {
+    "revision": "e741918522270997b492",
+    "url": "/weather/static/js/runtime-main.3b601d73.js"
+  },
+  {
+    "revision": "7292554078803de8e0f2092fd70620c8",
+    "url": "/weather/static/media/cloudy.72925540.svg"
+  },
+  {
+    "revision": "7ee35f1fcdf4cec40cdeef654ba5e871",
+    "url": "/weather/static/media/mostly_cloudy.7ee35f1f.svg"
+  },
+  {
+    "revision": "34484b548de422bd8b17b1e7abe99359",
+    "url": "/weather/static/media/partly_cloudy.34484b54.svg"
+  },
+  {
+    "revision": "0bcd5b3fe2f64e71101146292ce96ff9",
+    "url": "/weather/static/media/partly_cloudy_windy.0bcd5b3f.svg"
+  },
+  {
+    "revision": "633504b6d3a6ff981c9627e404912e12",
+    "url": "/weather/static/media/rain.633504b6.svg"
+  },
+  {
+    "revision": "7f879cddaf0643746b2693465da5a7be",
+    "url": "/weather/static/media/snow.7f879cdd.svg"
+  },
+  {
+    "revision": "c536ba251d13bb36c588a2e02cf10075",
+    "url": "/weather/static/media/sun.c536ba25.svg"
+  },
+  {
+    "revision": "98a1376443c336639604f2a36ae98ee0",
+    "url": "/weather/static/media/sun_thunderstorm.98a13764.svg"
+  },
+  {
+    "revision": "bdf06b849d70fc19da86de5589fc801e",
+    "url": "/weather/static/media/sunny_rain.bdf06b84.svg"
+  },
+  {
+    "revision": "48b7e66b61fc15a99116bbf2421bbf71",
+    "url": "/weather/static/media/thunderstorm.48b7e66b.svg"
+  }
+]);
